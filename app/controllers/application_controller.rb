2810af86666bf91b151fd1f121121b4a5c1c9b17
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-08-15 18:42:24 +0200

    display a notice about authentication denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   include Twitter::Login::Helpers
   include Facebook::Login::Helpers
   
+  before_filter :authentication_denied_notice
+  
   def logged_in?
     !!current_user
   end
@@ -13,4 +15,15 @@ class ApplicationController < ActionController::Base
     @current_user ||= twitter_user && User.find_or_create_from_twitter(twitter_user)
   end
   helper_method :current_user
+  
+  protected
+  
+  def authentication_denied_notice
+    %w[twitter facebook].detect do |service|
+      if session[:"#{service}_error"] == 'user_denied'
+        session.delete(:"#{service}_error")
+        flash.now[:warning] = "You have refused to connect with #{service.titleize}"
+      end
+    end
+  end
 end
