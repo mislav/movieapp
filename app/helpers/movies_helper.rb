9321fc254e48b2deb6414517c2683a933be199f7
Ivana Vasilj <ivasilj@gmail.com>
2010-09-02 22:23:20 +0200

    new style

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,12 @@
 module MoviesHelper
   def link_to_movie(movie)
-    link_to(title_for_movie(movie), movie) + " (#{movie.year})"
+    link_to(title_for_movie(movie), movie) + movie_year(movie)
+  end
+  
+  def movie_year(movie)
+    if movie.year.blank? then ""
+    else " <span>(<time>#{movie.year}</time>)</span>".html_safe
+    end
   end
   
   def title_for_movie(movie)
