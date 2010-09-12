e9cec91f9c5b6039796cf26809b4dd556e781a25
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-09-12 22:54:52 +0200

    remove cookie secret from version control

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Movies::Application.config.secret_token = '37c5bb0acc2986b02e865ed722cecf5c7f451555e66c3ef870fff026d5aecccbaaf16f19d8657af25ccb0d7f897f0832213960fc9003f698812ebeb9eda7d92d'
