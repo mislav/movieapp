9f999baf1b3fac7b84fc81e803bf8ed137b5b765
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-02-16 21:55:12 +0100

    filter duplicate movies from TMDB search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -58,7 +58,7 @@ class Movie < Mingo
     self.tmdb_id = movie.id
     self.tmdb_url = movie.url
     self.tmdb_version = movie.version
-    self.imdb_id = movie.imdb_id
+    self.imdb_id = movie.imdb_id.presence
     
     # same name properties
     [:year, :runtime, :countries, :directors, :homepage].each do |property|
@@ -90,19 +90,47 @@ class Movie < Mingo
     EXTENDED.any? { |property| self[property].nil? }
   end
   
+  # filters out duplicate TMDB results by "imdb_id"
+  class IMDBUniqueFilter
+    def initialize
+      @imdb_ids = []
+    end
+    
+    def register(movie)
+      if movie.imdb_id.blank? or not registered? movie
+        yield movie
+        @imdb_ids << movie.imdb_id if movie.imdb_id.present?
+      end
+    end
+    
+    def registered?(movie)
+      @imdb_ids.include? movie.imdb_id
+    end
+    
+    def process(movies)
+      movies.each_with_object([]) do |movie, all|
+        register(movie) { all << movie }
+      end
+    end
+  end
+  
   def self.search(term)
     tmdb_movies = Tmdb.search(term).movies
     netflix_titles = Netflix.search(term, :expand => ['synopsis']).titles
+    filter = IMDBUniqueFilter.new
     
     [].tap do |movies|
       netflix_titles.each do |netflix_title|
         if tmdb_movie = tmdb_movies.find { |m| m == netflix_title }
           tmdb_movies.delete(tmdb_movie)
-          movies << new(:tmdb_movie => tmdb_movie, :netflix_title => netflix_title)
+          filter.register(tmdb_movie) do
+            movies << new(:tmdb_movie => tmdb_movie, :netflix_title => netflix_title)
+          end
         end
       end
       
-      movies.concat(from_tmdb_movies(tmdb_movies)).each(&:save)
+      other_movies = from_tmdb_movies filter.process(tmdb_movies)
+      movies.concat(other_movies).each(&:save)
     end
   end
 
