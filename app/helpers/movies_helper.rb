e12b5361f352f696cf62e5532ccdbe6467899ec4
Ivana Vasilj <ivasilj@gmail.com>
2011-02-18 00:55:40 +0100

    display original movie title and English translation on separate lines

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,10 +4,6 @@ module MoviesHelper
     pluralize collection.send(method), 'movie'
   end
   
-  def link_to_movie(movie)
-    link_to(title_for_movie(movie), movie) + movie_year(movie)
-  end
-  
   def movie_elsewhere(movie)
     [ ["official website", :homepage],
       ["Wikipedia", :wikipedia_url],
@@ -33,14 +29,6 @@ module MoviesHelper
   def movie_year(movie)
     if movie.year.blank? then ""
     else %( <span class="year">(<time>#{movie.year}</time>)</span>).html_safe
-    end
-  end
-  
-  def title_for_movie(movie)
-    if movie.original_title and movie.original_title != movie.title
-      ("<i>%s</i> / %s" % [h(movie.original_title), h(movie.title)]).html_safe
-    else
-      movie.title
     end
   end
   
