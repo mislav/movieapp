bbc7cd0f5307d5a7c2745f39e533b41a1bfc14e9
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-02-20 16:33:18 +0100

    update escape_utils patch

--- config/initializers/escape_utils_patch.rb
+++ config/initializers/escape_utils_patch.rb
@@ -1,11 +1,7 @@
-module Rack
-  module Utils
-    def escape(s)
-      EscapeUtils.escape_url(s)
-    end
-  end
-end
-
 require 'escape_utils/html/rack'
-# require 'escape_utils/html/haml'
 require 'escape_utils/html/erb'
+require 'escape_utils/html/cgi'
+# require 'escape_utils/html/haml'
+
+# turn off the escaping of the '/' character during HTML escaping
+# EscapeUtils.html_secure = false
