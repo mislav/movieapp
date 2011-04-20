16efd69e649537e40562b0b578d4658229c49566
Ivana Vasilj <ivasilj@gmail.com>
2011-04-19 22:08:09 -0700

    add js code for emulating HTML5 "placeholder" and "autofocus" attributes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -137,3 +137,48 @@ if (pagination) {
     }
   })
 }
+
+// Emulates HTML5 "placeholder" and "autofocus" attributes.
+// from https://github.com/teambox/teambox/blob/master/app/javascripts/html5.js
+document.on('dom:loaded', function() {
+  if (!window.Modernizr) return
+  
+  if (!Modernizr.input.placeholder) {
+    var selector = 'input[placeholder], textarea[placeholder]'
+    
+    function emulatePlaceholder(input) {
+      var val = input.getValue(), text = input.readAttribute('placeholder')
+      if (val.empty() || val === text)
+        input.setValue(text).addClassName('placeholder')
+    }
+    
+    document.on('focusin', selector, function(e, input) {
+      if (input.getValue() === input.readAttribute('placeholder'))
+        input.setValue('').removeClassName('placeholder')
+    })
+    
+    document.on('focusout', selector, function(e, input) {
+      emulatePlaceholder(input)
+    })
+    
+    // setup existing fields
+    $$(selector).each(emulatePlaceholder)
+    
+    // observe form submits and clear emulated placeholder values
+    $(document.body).on('submit', 'form:has(' + selector + ')', function(e, form) {
+      form.select(selector).each(function(field) {
+        if (field.getValue() == field.readAttribute('placeholder')) field.setValue('')
+      })
+    })
+    
+    // observe new forms inserted into document and setup fields inside
+    document.on('DOMNodeInserted', 'form', function(e) {
+      if (e.element().match('form')) e.element().select(selector).each(emulatePlaceholder)
+    })
+  }
+
+  if (!Modernizr.input.autofocus) {
+    var input = $(document.body).down('input[autofocus]')
+    if (input) input.activate()
+  }
+})
