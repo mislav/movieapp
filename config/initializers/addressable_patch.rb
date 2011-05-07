900fcc0b589d9a1b30a9c174a89738b5c07e2dbd
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-05-07 00:58:08 -0700

    test if addressable patch is necessary before applying

--- config/initializers/addressable_patch.rb
+++ config/initializers/addressable_patch.rb
@@ -1,17 +1,21 @@
 require 'addressable/uri'
 
-# fix `normalized_query` by sorting query key-value pairs
-class Addressable::URI
-  class << self
-    alias old_normalize_component normalize_component
+# feature-detect the bug
+unless Addressable::URI.parse('/?a=1&b=2') === '/?b=2&a=1'
+  # fix `normalized_query` by sorting query key-value pairs
+  # (rejected: https://github.com/sporkmonger/addressable/issues/28)
+  class Addressable::URI
+    class << self
+      alias normalize_component_without_query_fix normalize_component
     
-    def normalize_component(component, character_class = CharacterClasses::RESERVED + CharacterClasses::UNRESERVED)
-      normalized = old_normalize_component(component, character_class)
-      if character_class == Addressable::URI::CharacterClasses::QUERY
-        pairs = normalized.split('&').sort_by { |pair| pair[0, pair.index('=') || pair.length] }
-        normalized = pairs.join('&')
+      def normalize_component(component, character_class = CharacterClasses::RESERVED + CharacterClasses::UNRESERVED)
+        normalized = normalize_component_without_query_fix(component, character_class)
+        if character_class == Addressable::URI::CharacterClasses::QUERY
+          pairs = normalized.split('&').sort_by { |pair| pair[0, pair.index('=') || pair.length] }
+          normalized = pairs.join('&')
+        end
+        normalized
       end
-      normalized
     end
   end
 end
