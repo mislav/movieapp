db1d5eebd8e13b5871d040f5e5e0d81443a05c3e
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-05-23 03:28:15 +0200

    tweak boot.rb script

--- config/boot.rb
+++ config/boot.rb
@@ -1,13 +1,15 @@
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+if File.exist? ENV['BUNDLE_GEMFILE']
   require 'bundler'
-  Bundler.setup
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
+  begin
+    Bundler.setup
+  rescue Bundler::GemNotFound => e
+    STDERR.puts e.message
+    STDERR.puts "Try running `bundle install`."
+    exit!
+  end 
+end
