a71f13821cc1cacb7a486a9d1243a9ccd8303e80
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-10-23 21:21:17 -0400

    New production configuration for Unicorn
    - unix socket is more performant
    - 4 workers processes was too much for 512MB RAM machine

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,7 @@
-if "production" == ENV['RACK_ENV']
-  worker_processes 4
+if "production" == ENV["RAILS_ENV"]
+  listen "/tmp/unicorn.movieapp.sock", :backlog => 64
+  pid "tmp/pids/unicorn.pid"
+  worker_processes 2
   timeout 30
 else
   worker_processes 1
