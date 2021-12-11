4f9161c0b05e934fbbc83e2acd3d6583fcb1f2ab
Mislav Marohnić <git@mislav.net>
2021-12-11 18:06:54 +0100

    Avoid BigDecimal problem

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,13 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require 'bigdecimal'
+class BigDecimal
+    class << self
+        def new(val)
+            BigDecimal(val)
+        end
+    end
+end
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
